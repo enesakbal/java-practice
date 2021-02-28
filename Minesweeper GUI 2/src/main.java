4b7ced986bd719bfa14699520acb90b5b927ad92
enesakbal <60822023+enesakbal@users.noreply.github.com>
2021-02-28 20:42:04 +0300

    Minesweeper with Swing (GUI)

---
+++ Minesweeper GUI 2/src/main.java
@@ -0,0 +1,16 @@
+
+public class main {
+    
+    
+    public static void main(String[] args) {
+        
+        Game game1 = new Game();
+        game1.play();
+        
+
+        
+        
+    }
+    
+    
+}
